c1fb88128bf6eedd77872d1ef15c016697179680
Joshua Adela <joshuaadela@gmail.com>
2020-06-13 17:02:00 +0800

    After Section 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-course';
+  onDivClick(){
+    alert('Hello World');
+  }
+  onKeyUp(newTitle:string){
+    this.title = newTitle;
+  }
 }
